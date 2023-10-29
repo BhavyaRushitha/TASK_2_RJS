316c80d3a7a9375d95134695ddbf0a8488f68c9a
BhavyaRushitha <123400543+BhavyaRushitha@users.noreply.github.com>
2023-10-29 16:19:11 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Caluclator from "./Calculator";
+function App() {
+  return (
+    <div className="App">
+      <Caluclator />
+    </div>
+  );
+}
+
+export default App;
